6e0814193e10bf55b823dfc8ec99e49a8656963d
Markus Ahlstrand <markus@ahlstrand.de>
2022-02-22 17:26:34 +0100

    chore: remove old files

--- examples/wrangler/webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  target: "webworker",
-  entry: "./index.js",
-  mode: "production",
-  optimization: {
-    // We no not want to minimize our code.
-    minimize: false,
-  },
-};
